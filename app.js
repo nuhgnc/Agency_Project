e8fb8ed501923c498a0667e5c3b5f10376fcd6cf
nuh genc <66550954+nuhgnc@users.noreply.github.com>
2022-09-23 16:18:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -22,5 +22,5 @@ app.use(homeRoutes);
 
 
 const port = process.env.PORT || 3000
-app.listen(port, () => console.log('server started on 3000'));
+app.listen(port, () => console.log('server started on ', port));
 
